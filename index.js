d82cd7ce45776197cbe11248b0270a3269f32159
aungtintlwin <go2lwin@yahoo.com>
2019-05-29 20:33:36 -0700

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function myFunction() {
-    this.style.background="#000000";
-}
+
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+
+        let square = document.querySelector('#square')
+
+        let button = document.querySelector('#changeColor')
+        
+        button.addEventListener('click', () => {
+
+            square.style.backgroundColor = getRandomColor()
+
+        })
+        
+    
+    })
